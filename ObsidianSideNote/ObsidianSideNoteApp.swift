08332cdda1530dc136326a1d20d79f8ae7b04e5b
Luke Smith <lukesmith@live.com>
2025-11-27 11:54:28 -0800

    Initial Commit

---
+++ ObsidianSideNote/ObsidianSideNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObsidianSideNoteApp.swift
+//  ObsidianSideNote
+//
+//  Created by Luke  on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ObsidianSideNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObsidianSideNoteTests/ObsidianSideNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  ObsidianSideNoteTests.swift
+//  ObsidianSideNoteTests
+//
+//  Created by Luke  on 11/27/25.
+//
+
+import Testing
+@testable import ObsidianSideNote
+
+struct ObsidianSideNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
